f79f6ac9fe0fe93a2e7140919c4fae627f77217a
aristides1000 <arsitides1000@gmail.com>
2023-08-21 09:10:54 -0400

    feat: add the navbar in the project

--- index.php
+++ index.php
@@ -1,24 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>To Do List</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-  <link rel="stylesheet" href="node_modules/bootstrap-icons/font/bootstrap-icons.min.css">
-  <link rel="stylesheet" href="style.css">
-</head>
-<body>
-  <header>
-    <nav class="navbar bg-body-tertiary">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">
-          <i class="bi bi-bell-fill"></i>
-          Pagina de tareas 
-        </a>
-      </div>
-    </nav>
-  </header>
+<?php
+  include("includes/header.php");
+?>
 
   <main>
 
