14316592a2920c3be0990140d9b3c08e1f906d07
aristides1000 <arsitides1000@gmail.com>
2023-08-23 10:50:12 -0400

    feat: add messaes

--- index.php
+++ index.php
@@ -1,9 +1,21 @@
 <?php
+  include("database/db.php");
+?>
+
+<?php
   include("includes/header.php");
 ?>
 
-  <main>
+  <main class="container p-4">
+    <div class="row">
+      <div class="col-md-4">
+        <!-- MESSAGES -->
 
+        <?php if(isset($_SESSION['message']))?>
+        <div class="alert alert-<?= $_SESSION['message_type']?> alert-dismissible fade show" role="alert"></div>
+        
+      </div>
+    </div>
   </main>
 
 <?php
