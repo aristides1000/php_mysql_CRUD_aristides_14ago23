7c08dba8c03c8579fa79fae75510c27e53dfbe0e
aristides1000 <aristides1000@gmail.com>
2023-08-25 09:56:17 -0400

    fix: issue with upload image

--- save_task.php
+++ save_task.php
@@ -5,11 +5,23 @@
     $title = $_POST['title'];
     $description = $_POST['description'];
     $image = getimagesize($_FILES['image']['tmp_name']);
-    if($image !== false) {
+    if($image !== false){
       $image = $_FILES['image']['tmp_name'];
       $img_content = addslashes(file_get_contents($image));
     } else {
       $img_content = "";
     }
+
+    $query = "INSERT INTO task(title, description, image) VALUES('$title', '$description', '$img_content')";
+    $result = mysqli_query($conn, $query);
+
+    if(!$result) {
+      die("Query Failed.");
+    }
+
+    $_SESSION['message'] = 'Task Saved Successfully';
+    $_SESSION['message_type'] = 'success';
+    header('Location: index.php');
+
   }
 ?>
